6b098ae6df6115f1772f819bae6279693889093a
Justin Huntington <corusco@gmail.com>
2015-04-16 21:58:19 -0600

    Added Project Code

--- FizzBuzz-Objc/FizzBuzz-Objc/AppDelegate.m
+++ FizzBuzz-Objc/FizzBuzz-Objc/AppDelegate.m
@@ -13,9 +13,59 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     
-    // Insert code here
+    //Loop all integers from 1-100, checking against methods.
+    for (int i = 1; i<=100; i++){
+        BOOL buzzed = [self isBuzzed:i];
+        BOOL fizzed = [self isFizzed:i];
+    
+        //Print Buzz if meets appropriate arguments
+        if (buzzed && !fizzed){
+            NSLog(@"Buzz (%d)", i);
+        }
+        
+        //Print Fizz if meets appropriate arguments
+        else if (!buzzed && fizzed){
+            NSLog(@"Fizz (%d)", i);
+        }
+    
+        //Print FizzBuzz if it meets both above arguments
+        else if (buzzed && fizzed){
+            NSLog(@"FizzBuzz! (%d)", i);
+        }
+        
+        //nothing there. Just print the integer.
+        else {
+            NSLog(@"%d", i);
+        }
+    }
     
     return YES;
+}
+
+//Method to check to see if integer is divisible by 3, or contains the numeral 3. If so, it's buzzy.
+-(BOOL) isBuzzed:(int)integer{
+    if (integer % 3 == 0) {
+        return YES;
+    }
+    else if ([[NSString stringWithFormat:@"%d", integer] containsString:@"3"]){
+        return YES;
+    }
+    else {
+        return NO;
+    }
+}
+
+//Method to check to see if integer is divisible by 5, or contains the numeral 5. If so, it's fizzy.
+-(BOOL) isFizzed:(int)integer;{
+    if (integer % 5 == 0) {
+        return YES;
+    }
+    else if ([[NSString stringWithFormat:@"%d", integer] containsString:@"5"]){
+        return YES;
+    }
+    else {
+        return NO;
+    }
 }
 
 @end
